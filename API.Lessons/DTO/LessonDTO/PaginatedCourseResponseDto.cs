ebbba536851374ee42a022bdf0cc64eb1ee0da00
EfVictor <efimov840@gmail.com>
2026-02-16 13:03:38 +0300

    Add gitignore

--- API.Lessons/DTO/LessonDTO/PaginatedCourseResponseDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace API.Lessons.DTO.LessonDTO;
-
-public class PaginatedCourseResponseDto  
-{
-    public int CurrentPage { get; set; } 
-    public int TotalPages { get; set; }
-}
--- API.Lessons/Models/FullLessonTask.cs
+++
@@ -1,9 +0,0 @@
-﻿using API.Lessons.DTO.LessonDTO;
-
-namespace API.Lessons.Models;
-
-public class FullLessonTask  
-{
-    public List<CourseCategory> Categories { get; set; } = new List<CourseCategory>();
-    public PaginatedCourseResponseDto Pagination { get; set; }  
-}
